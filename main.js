92dd0266f46bb876a72fb74e555fabce9fec8dac
TURBOMATIZ <149716136+TURBOMATIZ@users.noreply.github.com>
2023-11-28 19:10:48 +0100

    nav

---
+++ main.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector(".nav-links");
+
+let button = document.querySelector('.btn');
+
+button.addEventListener('click',function(event){
+    console.log(event.target)
+});

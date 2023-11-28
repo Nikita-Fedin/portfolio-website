af81dc45e889fe8e170f182245328b941c1c4f87
TURBOMATIZ <149716136+TURBOMATIZ@users.noreply.github.com>
2023-11-28 20:28:03 +0100

    linki

--- main.js
+++ main.js
@@ -1,7 +1,41 @@
-let menu = document.querySelector(".nav-links");
+let classList = '.main-link'
+let menu = document.querySelector(".nav-links");    
 
-let button = document.querySelector('.btn');
 
-button.addEventListener('click',function(event){
-    console.log(event.target)
+menu.addEventListener('click',function(event){
+    let clickTarget = event.target
+    let activeBtn = document.querySelector('.active')
+
+    if (clickTarget.classList.contains('nav-link')){
+        activeBtn.classList.remove('active');
+        clickTarget.classList.add('active');
+    }
 });
+
+window.onscroll = function() {
+    let h = document.documentElement.clientHeight   
+    if (window.scrollY >= h*4 ){
+        classLink = '.comments-link';
+    }
+    else if (window.scrollY >= h*3){
+        classLink = '.works-link';
+    }
+    else if (window.scrollY >= h*2){
+        classLink = '.skills-link';
+    }
+    else if (window.scrollY >= h){
+        classLink = '.about-link';
+    }
+    else{
+        classLink = '.main-link';
+    }
+
+    
+    let activeBtn = document.querySelector('.active');
+    let NewActiveBtn = document.querySelector(classLink);
+    if (!NewActiveBtn.classList.contains('active')){
+        NewActiveBtn.classList.add('active');
+        activeBtn.classList.remove('active')
+    }
+};
+
